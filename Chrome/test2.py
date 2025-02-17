8f8852acea10ae7b02e356f85124079b3db9c470
IvanVl94 <123305766+IvanVl94@users.noreply.github.com>
2025-02-17 21:47:36 +0300

    Исправленное ДЗ 5 вариант 3

--- Chrome/test2.py
+++ Chrome/test2.py
@@ -1,9 +1,7 @@
 from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
 
@@ -21,4 +19,4 @@ blue_button.click()
 
 sleep(4)
 
-driver.quit
+driver.quit()
--- Mozila/Moz1.py
+++ Mozila/Moz1.py
@@ -1,7 +1,6 @@
 
 from time import sleep
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
